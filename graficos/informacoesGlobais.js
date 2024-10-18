6d95d5e67e449b9ed3d5f0a73f453ad1ba44f928
diogocravelin <129780312+diogocravelin@users.noreply.github.com>
2024-10-18 09:10:32 -0300

    correção de erros

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,7 +9,7 @@ async function visualizarInfomacoesGlobais() {
     const minutos = Math.round((dados.tempo_medio - horas)*100)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo}</span> de pessoas e que aproximadamente <span> ${pessoasConectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} </span> bilhões de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${horas} </span> horas e <span>${minutos}</span> minutos conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
